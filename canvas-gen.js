29be1a130c2965ee97ad5bb6f82dc5f940d44c98
u2re-dev <c24b@u2re.ru>
2025-04-04 22:48:33 +0700

    small optimize, progressive DOM building

--- canvas-gen.js
+++ canvas-gen.js
@@ -1,12 +1,14 @@
-const generateCSSCanvas = (root, w, h)=>{
+const generateCSSCanvas = async (root, w, h)=>{
     root.style.setProperty("--w", `${w}`);
     root.style.setProperty("--h", `${h}`);
+    root.inert = true;
     for (let y = 0; y < h; y++) {
         const row = document.createElement("div");
         row.classList.add("row");
         row.style.setProperty("--y", `${y}`);
         row.style.setProperty("--w", `${w}`);
         row.dataset.y = `${y}`;
+        row.inert = true;
         for (let x = 0; x < w; x++) {
             const pixel = document.createElement("div");
             pixel.classList.add("column");
@@ -14,9 +16,13 @@ const generateCSSCanvas = (root, w, h)=>{
             pixel.dataset.y = `${y}`;
             pixel.dataset.x = `${x}`;
             pixel.style.setProperty("--x", `${x}`);
+            pixel.inert = true;
             row.appendChild(pixel);
         }
         root.appendChild(row);
+
+        // avoid overpaint and overload when building DOM
+        await new Promise((r)=>requestAnimationFrame(r));
     }
 }
 
