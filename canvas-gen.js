417961db10a634f3d75ab8cdc1917ab20481d458
u2re-dev <c24b@u2re.ru>
2025-04-09 21:02:48 +0700

    rendering bit faster (less memory)

--- canvas-gen.js
+++ canvas-gen.js
@@ -3,19 +3,20 @@ const generateCSSCanvas = async (root, w, h)=>{
     root.style.setProperty("--h", `${h}`);
     root.inert = true;
     for (let y = 0; y < h; y++) {
-        const row = document.createElement("div");
-        row.classList.add("row");
-        row.style.setProperty("--y", `${y}`);
-        row.style.setProperty("--w", `${w}`);
-        row.dataset.y = `${y}`;
-        row.inert = true;
+        const row = document.createDocumentFragment();
+        //row.classList.add("row");
+        //row.style.setProperty("--y", `${y}`);
+        //row.style.setProperty("--w", `${w}`);
+        //row.dataset.y = `${y}`;
+        //row.inert = true;
         for (let x = 0; x < w; x++) {
             const pixel = document.createElement("div");
-            pixel.classList.add("column");
-            pixel.classList.add("pixel");
-            pixel.dataset.y = `${y}`;
-            pixel.dataset.x = `${x}`;
+            //pixel.classList.add("column");
+            //pixel.classList.add("pixel");
+            //pixel.dataset.y = `${y}`;
+            //pixel.dataset.x = `${x}`;
             pixel.style.setProperty("--x", `${x}`);
+            pixel.style.setProperty("--y", `${y}`);
             pixel.inert = true;
             row.appendChild(pixel);
         }
